51f0a3f7137e476adbfd5b0cf6e4d06abab3051d
Alfonso Tatarelli <a.tatarelli@gmail.com>
2026-07-01 23:31:19 +0200

    chore(shared-ui): header licenza generico, rimossi riferimenti GovPay
    Uniforma l'header di licenza di tutti i file .ts della libreria
    @linkit/shared-ui all'header generico Link.it (Copyright + GPL v3 completo,
    con i paragrafi WITHOUT ANY WARRANTY / You should have received). Rimosse le
    righe "GovPay - Porta di Accesso al Nodo dei Pagamenti SPC" e il link gov4j:
    la libreria è generica e non deve riferirsi allo specifico applicativo, a
    differenza dei file dell'app che mantengono l'header GovPay.
    
    Solo commenti di licenza: nessun impatto funzionale (tsc OK, suite verde).
    NB: la lib è copia mirror → policy da allineare upstream [GOVPAY]/govpay-console.

--- projects/lnk-shared-ui/src/lib/components/search-pill/search-chip.component.ts
+++ projects/lnk-shared-ui/src/lib/components/search-pill/search-chip.component.ts
@@ -1,7 +1,4 @@
 /*
- * GovPay - Porta di Accesso al Nodo dei Pagamenti SPC
- * http://www.gov4j.it/govpay
- *
  * Copyright (c) 2014-2026 Link.it srl (http://www.link.it).
  *
  * This program is free software: you can redistribute it and/or modify
